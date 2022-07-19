968283fa73840e65b4314ba9b40ef02cffb7f976
sim511777 <sim51177@gmail.com>
2022-07-19 09:15:09 +0900

    폴더 선택, 누겟 정리

--- TinyPak/FormMain.cs
+++ TinyPak/FormMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +23,32 @@ namespace TinyPak {
             var r = dlg.ShowDialog(this.Handle);
             if (r != CommonFileDialogResult.Ok)
                 return;
-            foreach (var file in dlg.FileNames) {
-                Console.WriteLine(file);
+            
+            var dir = new DirectoryInfo(dlg.FileName);
+            List<byte> buffer = new List<byte>();
+            PakDirectory(dir, buffer);
+        }
+
+        private void PakDirectory(DirectoryInfo parentDir, List<byte> buffer) {
+            var dirs = parentDir.GetDirectories();
+            var files = parentDir.GetFiles();
+
+            buffer.AddRange(BitConverter.GetBytes(dirs.Length));    // 폴더 개수 인코딩
+            buffer.AddRange(BitConverter.GetBytes(files.Length));   // 파일 개수 인코딩
+            foreach (var dir in dirs) {
+                PakDirectoryInfo(dir, buffer);  // 폴더 정보 인코딩
             }
+            foreach (var file in files) {
+                PakFileInfo(file, buffer);      // 파일 정보 인코딩
+            }
+        }
+
+        private void PakDirectoryInfo(DirectoryInfo dir, List<byte> buffer) {
+            throw new NotImplementedException();
+        }
+
+        private void PakFileInfo(FileInfo file, List<byte> buffer) {
+            throw new NotImplementedException();
         }
 
         private void unpakToolStripMenuItem_Click(object sender, EventArgs e) {
